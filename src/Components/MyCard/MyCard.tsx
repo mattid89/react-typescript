8977c144642b462ff85c83b977d20b2fbae2ab3b
Matias Doto <matydoto@gmail.com>
2021-12-06 13:19:13 -0300

    jugando con las cards de UI Material

--- src/Components/MyCard/MyCard.tsx
+++ src/Components/MyCard/MyCard.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import { Button } from '@mui/material'
+import { Box, Button, Card, CardActions, CardContent, Typography } from '@mui/material'
 // import AccessibilityOutlinedIcon from '@mui/icons-material/AccessibilityOutlined';
 
 const MyCard = ({ title, subtitle }: { title: string, subtitle: string }) => {
@@ -11,18 +11,68 @@ const MyCard = ({ title, subtitle }: { title: string, subtitle: string }) => {
 
       const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => setNumero(numero + 1)
 
+      // const cardStyle = {
+      //       color: '#333',
+      //       border: '1px solid gray',
+      //       borderRadius: '3px'
+      // }
+
+      const bull = (
+            <Box
+              component="span"
+              sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+            >
+              •
+            </Box>
+          );
 
       return (
-            <div className="flex-column padding">
-                  <h4>{title}</h4>
-                  <div>{subtitle}</div>
-                  <Button 
-                        onClick={(event) => handleClick(event)} 
-                        variant="contained"
-                        >Apretar {numero} 
-                  </Button>
-                        {/* <svg data-testid="AccessibilityOutlinedIcon"></svg> */}
-            </div>
+            // <div 
+            //       // style={cardStyle} 
+            //       className="flex-column padding"
+            // >
+
+            //       <h4>{title}</h4>
+            //       <div>
+            //             <p>{subtitle}</p>
+            //       </div>
+            //       <Button 
+            //             onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => handleClick(event)} 
+            //             variant="contained"
+            //             >Apretar {numero} 
+            //       </Button>
+            //             {/* <svg data-testid="AccessibilityOutlinedIcon"></svg> */}
+            // </div>
+
+            <Card sx={{ minWidth: 275 }}>
+
+                  <CardContent>
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                              Word of the Day
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                              be{bull}nev{bull}o{bull}lent
+                        </Typography>
+                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                              adjective
+                        </Typography>
+                        <Typography variant="body2">
+                              well meaning and kindly.
+                              <br />
+                              {'"a benevolent smile"'}
+                        </Typography>
+                  </CardContent>
+
+                  <CardActions>
+                        <Button
+                              onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => handleClick(event)}
+                              variant="contained"
+                        >
+                              Apretar {numero}
+                        </Button>
+                  </CardActions>
+
+            </Card>
       )
 }
 
