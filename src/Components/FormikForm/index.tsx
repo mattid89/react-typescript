3d9b4c899457fe8db7c5de1d65e637cb87446aff
matias.doto <matydoto@gmail.com>
2022-06-30 18:27:29 -0300

    uso useFormik

--- src/Components/FormikForm/index.tsx
+++ src/Components/FormikForm/index.tsx
@@ -1,7 +1,14 @@
 import { useFormik } from "formik"
 
 const FormikForm = () => {
-	// const formik = useFormik()
+	const formik = useFormik({
+		initialValues: {
+			name: '',
+			lastname: '',
+			email: ''
+		},
+		onSubmit: value => console.log(value)
+	})
 }
 
 export default FormikForm
